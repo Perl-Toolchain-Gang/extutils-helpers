9b498802bb02506bc03134f3b12b193a16d667d8
Christian Walde <walde.christian@googlemail.com>
2013-04-12 13:09:02 +0200

    remove exit jump that masks the return value

--- lib/ExtUtils/Helpers/Windows.pm
+++ lib/ExtUtils/Helpers/Windows.pm
@@ -47,7 +47,6 @@ sub _pl2bat {
     perl $opts{ntargs}
     if NOT "%COMSPEC%" == "%SystemRoot%\\system32\\cmd.exe" goto endofperl
     if %errorlevel% == 9009 echo You do not have Perl in your PATH.
-    if errorlevel 1 goto script_failed_so_exit_with_non_zero_val 2>nul
     goto endofperl
     \@rem ';
 EOT
