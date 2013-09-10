1b9ec492300f2724fe4eae308636e04ad58409ed
Leon Timmermans <fawaka@gmail.com>
2013-09-10 13:39:20 +0200

    Only use layers on 5.8+

--- lib/ExtUtils/Helpers/Unix.pm
+++ lib/ExtUtils/Helpers/Unix.pm
@@ -9,14 +9,16 @@ use Carp qw/croak/;
 use Config;
 use Text::ParseWords 3.24 qw/shellwords/;
 
+my $layer = $] >= 5.008001 ? ":raw" : "";
+
 sub make_executable {
 	my $filename = shift;
 	my $current_mode = (stat $filename)[2] + 0;
 	if (-T $filename) {
-		open my $fh, '<:raw', $filename;
+		open my $fh, "<$layer", $filename;
 		my @lines = <$fh>;
 		if (@lines and $lines[0] =~ s{ \A \#! \s* (?:/\S+/)? perl \b (.*) \z }{$Config{startperl}$1}xms) {
-			open my $out, '>:raw', "$filename.new" or croak "Couldn't open $filename.new: $!";
+			open my $out, ">$layer", "$filename.new" or croak "Couldn't open $filename.new: $!";
 			print $out @lines;
 			close $out;
 			rename $filename, "$filename.bak" or croak "Couldn't rename $filename to $filename.bak";
