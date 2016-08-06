957d3860e3e8e4fdf81566e0a70d4361d1b23eb3
Leon Timmermans <fawaka@gmail.com>
2016-08-06 19:00:21 +0200

    Make split_like_shell always unixy
    The code was assuming windows specific quoting was appropriate, but in our
    particular case (PERL_MB_OPTS) that is not helpful at all.

--- lib/ExtUtils/Helpers.pm
+++ lib/ExtUtils/Helpers.pm
@@ -6,6 +6,7 @@ use Exporter 5.57 'import';
 use Config;
 use File::Basename qw/basename/;
 use File::Spec::Functions qw/splitpath canonpath abs2rel splitdir/;
+use Text::ParseWords 3.24 ();
 
 our @EXPORT_OK = qw/make_executable split_like_shell man1_pagename man3_pagename detildefy/;
 
@@ -15,6 +16,16 @@ BEGIN {
 	my $impl = $impl_for{$^O} || 'Unix';
 	require "ExtUtils/Helpers/$impl.pm";
 	"ExtUtils::Helpers::$impl"->import();
+}
+
+sub split_like_shell {
+	my ($string) = @_;
+
+	return if not defined $string;
+	$string =~ s/^\s+|\s+$//g;
+	return if not length $string;
+
+	return Text::ParseWords::shellwords($string);
 }
 
 sub man1_pagename {
--- lib/ExtUtils/Helpers/Windows.pm
+++ lib/ExtUtils/Helpers/Windows.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings FATAL => 'all';
 
 use Exporter 5.57 'import';
-our @EXPORT = qw/make_executable split_like_shell detildefy/;
+our @EXPORT = qw/make_executable detildefy/;
 
 use Config;
 use Carp qw/carp croak/;
@@ -104,61 +104,6 @@ EOT
 	close $out;
 
 	return $opts{out};
-}
-
-sub split_like_shell {
-	# As it turns out, Windows command-parsing is very different from
-	# Unix command-parsing.	Double-quotes mean different things,
-	# backslashes don't necessarily mean escapes, and so on.	So we
-	# can't use Text::ParseWords::shellwords() to break a command string
-	# into words.	The algorithm below was bashed out by Randy and Ken
-	# (mostly Randy), and there are a lot of regression tests, so we
-	# should feel free to adjust if desired.
-
-	local ($_) = @_;
-
-	my @argv;
-	return @argv unless defined && length;
-
-	my $arg = '';
-	my ($i, $quote_mode ) = ( 0, 0 );
-
-	while ( $i < length ) {
-
-		my $ch      = substr $_, $i, 1;
-		my $next_ch = substr $_, $i+1, 1;
-
-		if ( $ch eq '\\' && $next_ch eq '"' ) {
-			$arg .= '"';
-			$i++;
-		} elsif ( $ch eq '\\' && $next_ch eq '\\' ) {
-			$arg .= '\\';
-			$i++;
-		} elsif ( $ch eq '"' && $next_ch eq '"' && $quote_mode ) {
-			$quote_mode = !$quote_mode;
-			$arg .= '"';
-			$i++;
-		} elsif ( $ch eq '"' && $next_ch eq '"' && !$quote_mode &&
-				( $i + 2 == length() || substr( $_, $i + 2, 1 ) eq ' ' )
-			) { # for cases like: a"" => [ 'a' ]
-			push @argv, $arg;
-			$arg = '';
-			$i += 2;
-		} elsif ( $ch eq '"' ) {
-			$quote_mode = !$quote_mode;
-		} elsif ( $ch =~ /\s/ && !$quote_mode ) {
-			push @argv, $arg if $arg;
-			$arg = '';
-			++$i while substr( $_, $i + 1, 1 ) =~ /\s/;
-		} else {
-			$arg .= $ch;
-		}
-
-		$i++;
-	}
-
-	push @argv, $arg if defined $arg && length $arg;
-	return @argv;
 }
 
 sub detildefy {
